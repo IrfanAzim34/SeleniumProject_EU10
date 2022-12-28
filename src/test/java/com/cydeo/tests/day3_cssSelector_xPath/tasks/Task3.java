a3a1b921097687783c9fc6854c46a7109d9d321b
Irfan Azim <irfanteipov@gmail.com>
2022-12-29 01:49:36 +0300

    Created Files

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xPath/tasks/Task3.java
@@ -0,0 +1,41 @@
+package com.cydeo.tests.day3_cssSelector_xPath.tasks;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        // Open a Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+
+        // Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        /*
+        Verify “Log in” button text is as expected:
+        Expected: Log In
+         */
+        WebElement loginButton = driver.findElement(By.cssSelector("input[class = 'login-btn']"));
+        String actualButtonText = loginButton.getAttribute("value");
+        String expectButtonText = "Log In";
+
+        if (actualButtonText.equals(expectButtonText)){
+            System.out.println("PASS");
+            System.out.println("actualButtonText = " + actualButtonText);
+            System.out.println("expectButtonText = " + expectButtonText);
+        }else {
+            System.out.println("FAIL");
+            System.out.println("actualButtonText = " + actualButtonText);
+            System.out.println("expectButtonText = " + expectButtonText);
+        }
+
+        driver.close();
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xPath/tasks/Task4.java
@@ -0,0 +1,42 @@
+package com.cydeo.tests.day3_cssSelector_xPath.tasks;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+
+        //Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+
+        //Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+
+        /*
+        Verify “Reset password” button text is as expected:
+        Expected: Reset password
+         */
+        WebElement resetPasswordButton = driver.findElement(By.cssSelector("button[value='Reset password']"));
+
+        String expectResetPasswordText = "Reset password";
+        String actualResetPasswordText = resetPasswordButton.getText();
+
+        if (actualResetPasswordText.equals(expectResetPasswordText)){
+            System.out.println("PASS");
+            System.out.println("expectResetPasswordText = " + expectResetPasswordText);
+            System.out.println("actualResetPasswordText = " + actualResetPasswordText);
+        }else {
+            System.out.println("FAIL");
+            System.out.println("expectResetPasswordText = " + expectResetPasswordText);
+            System.out.println("actualResetPasswordText = " + actualResetPasswordText);
+        }
+
+        driver.close();
+
+    }
+
+}
