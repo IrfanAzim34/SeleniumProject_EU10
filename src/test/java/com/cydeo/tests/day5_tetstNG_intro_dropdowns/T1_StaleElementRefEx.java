01d2d765a8858ceda9939e0870f1ce45d13be00d
Irfan Azim <irfanteipov@gmail.com>
2023-01-06 00:40:43 +0300

    Created Files

---
+++ src/test/java/com/cydeo/tests/day5_tetstNG_intro_dropdowns/T1_StaleElementRefEx.java
@@ -0,0 +1,49 @@
+package com.cydeo.tests.day5_tetstNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_StaleElementRefEx {
+
+    public static void main(String[] args) {
+
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("Chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://practice.cydeo.com/add_remove_elements/
+        driver.get("https://practice.cydeo.com/add_remove_elements/");
+
+        //3. Click to “Add Element” button
+        WebElement addElementBtn = driver.findElement(By.xpath("//button[.='Add Element']"));
+        addElementBtn.click();
+
+        //4. Verify “Delete” button is displayed after clicking.
+        WebElement deleteBtn = driver.findElement(By.xpath("//button[.='Delete']"));
+        if (deleteBtn.isDisplayed())
+            System.out.println("Delete Button is displayed, PASS!");
+        else
+            System.out.println("Delete Button is not Displayed, FAIL!!!");
+
+        //5. Click to “Delete” button.
+        deleteBtn.click();
+
+        //6. Verify “Delete” button is NOT displayed after clicking.
+        try {
+            System.out.println("Delete Button Displayed = " + deleteBtn.isDisplayed());
+            System.out.println("Delete Button is Displayed, FAIL!!!");
+        }catch (StaleElementReferenceException e){
+            System.out.println("Delete Button is not Displayed, PASS!");
+        }
+
+        driver.close();
+
+    }
+
+}
---
+++ src/test/java/com/cydeo/tests/day5_tetstNG_intro_dropdowns/T3_RadioButton.java
@@ -0,0 +1,43 @@
+package com.cydeo.tests.day5_tetstNG_intro_dropdowns;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T3_RadioButton {
+
+    private static void clickAndVerifyRadioButton(WebDriver driver, String nameValue, String idValue){
+
+        List<WebElement> radioButtons = driver.findElements(By.name(nameValue));
+
+        for (WebElement radioButton : radioButtons) {
+
+            if (radioButton.getAttribute("id").equals(idValue)){
+
+                radioButton.click();
+                System.out.println(idValue + " is selected");
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+        driver.get("https://practice.cydeo.com/radio_buttons");
+        clickAndVerifyRadioButton(driver,"sport","hockey");
+
+
+        driver.close();
+
+    }
+
+}
